4401908a16e4db18b764bf0dc4378293eff00888
Marco Antonio Estelles <tonovarela@tonovarelamac.local>
2018-05-30 23:21:50 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Aplicacion de comandos
+Ejercicio de curso Udemy
+
+
+Se recuerda bajar los paquetes
+```
+npm install
+
+```
---
+++ por-hacer/por-hacer.js
@@ -0,0 +1,74 @@
+
+
+const fs = require('fs');
+
+
+
+let listadoPorHacer = [];
+
+const borrar = (descripcion) => {
+    cargarDB();
+    let index = listadoPorHacer.findIndex(tarea => tarea.descripcion === descripcion);
+    if (index >= 0) {
+        listadoPorHacer.splice(index, 1);
+        guardarDB();
+        return true
+    } else {
+        return false;
+    }
+}
+
+
+const actualizar = (descripcion, completado = true) => {
+    cargarDB();
+    let index = listadoPorHacer.findIndex(tarea => tarea.descripcion === descripcion);
+    if (index >= 0) {
+        listadoPorHacer[index].completado = completado;
+        guardarDB();
+        return true
+    } else {
+        return false;
+    }
+
+
+}
+
+const guardarDB = () => {
+    let data = JSON.stringify(listadoPorHacer);
+    fs.writeFile('./db/data.json', data, (err) => {
+        if (err) throw new Error("No se pudo grabar");
+
+    });
+};
+
+const cargarDB = () => {
+    try {
+        listadoPorHacer = require('../db/data.json');
+    } catch (error) {
+        listadoPorHacer = [];
+    }
+
+}
+const crear = (descripcion) => {
+    cargarDB();
+    let porHacer = {
+        descripcion,
+        completado: false
+    };
+    listadoPorHacer.push(porHacer);
+    guardarDB();
+    return porHacer;
+}
+
+const getListado = () => {
+    cargarDB();
+    return listadoPorHacer;
+
+}
+
+module.exports = {
+    crear,
+    getListado,
+    actualizar,
+    borrar
+}
